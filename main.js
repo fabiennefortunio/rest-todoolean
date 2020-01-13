3ec120394623003521371f73db73a0acc9a3adb1
Fabienne <fortunio.fabienne@gmail.com>
2020-01-13 19:04:51 +0100

    background image

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 
-var todoList=[];
+var list = $('todoList').append('todoList').val();
 
 
-function createList(){
-  month++;
-  if(month>11){
-      month=11;
-  }
-}
 
 //llamada ajax
 function createList(){
     $.ajax({
         'url': 'http://157.230.17.132:3008/',
-        // 'data': {
-        //
-        // },
-        'method': 'POST',
-        // 'dataType': 'json',
-        'success': function(e) {
+        'data': {
+            'text': todos,
+
+        },
+        'method': 'GET',
+        'dataType': 'json',
+        'success': function(data_response,status,hrq) {
             var i;
             if(status=="success"){
                 if(data_response.success){
-                    festivitiesList=data_response.response;
-                    for(i =0;i<festivitiesList.length;i++){
-                        festivitiesList[i].date=moment(festivitiesList[i].date);
-                    }
+                    loadList=data_response.response;
+
                 }
                 else{
                     console.log("Error en la peticion");
+                }
+            }
+            else{
+                console.log("Error en la comunicación con el servidor.");
+            }
+        }
+    });
+}
